8ba044fd2e936990a2695e56713e5912d6457bf9
diego939 <diegodavidalmiron1990@gmail.com>
2026-02-17 13:58:27 -0300

    fix add conection SQL and autentication methods

--- CapaPresentacionTienda/Program.cs
+++ CapaPresentacionTienda/Program.cs
@@ -1,7 +1,31 @@
+using CapaDatos;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+//AUTENTICACION METHODS -----------------------------------------------------------------------------------------
+using Microsoft.AspNetCore.Authentication.Cookies;
+//AUTENTICACION METHODS -----------------------------------------------------------------------------------------
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurar cadena de conexión
+Conexion.SetCadenaConexion(
+	builder.Configuration.GetConnectionString("cadenaSQL")!
+);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//AUTENTICACION METHODS -----------------------------------------------------------------------------------------
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+	.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+	{
+		options.LoginPath = "/Acceso/Index";
+		options.AccessDeniedPath = "/Acceso/Index";
+		options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+	});
+
+builder.Services.AddAuthorization();
+//AUTENTICACION METHODS -----------------------------------------------------------------------------------------
 
 var app = builder.Build();
 
@@ -15,11 +39,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
