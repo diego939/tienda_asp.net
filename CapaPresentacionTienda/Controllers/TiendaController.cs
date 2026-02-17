389f02dcfce57b2fe355bccd306de6e27401351f
diego939 <diegodavidalmiron1990@gmail.com>
2026-02-17 14:00:44 -0300

    fix add tienda controller

---
+++ CapaPresentacionTienda/Controllers/TiendaController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CapaPresentacionTienda.Controllers
+{
+	public class TiendaController : Controller
+	{
+		// GET: TiendaController
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		// GET: TiendaController/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: TiendaController/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: TiendaController/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: TiendaController/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
+
+		// POST: TiendaController/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: TiendaController/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		// POST: TiendaController/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}
