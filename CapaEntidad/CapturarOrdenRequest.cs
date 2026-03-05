e68f0da40ac6bb85759d33ff5b14df53d311cf81
diego939 <diegodavidalmiron1990@gmail.com>
2026-03-05 17:49:52 -0300

    fix add Class paypal orders

---
+++ CapaEntidad/CapturarOrdenRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+	public class CapturarOrdenRequest
+	{
+		public string contacto { get; set; }
+		public string telefono { get; set; }
+		public string direccion { get; set; }
+		public int idDepartamento { get; set; }
+		public string orderId { get; set; }
+
+	}
+}
---
+++ CapaEntidad/CrearOrdenRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+	public class CrearOrdenRequest
+	{
+		public decimal Total { get; set; }
+	}
+}
